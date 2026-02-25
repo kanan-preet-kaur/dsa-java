89124b009f10a48f5392331c4fc21758a55305c1
Kanan Preet Kaur <kananpreetkaur01@gmail.com>
2026-02-25 17:13:20 +0530

    Initial commit - Java DSA setup

---
+++ Java Programming Language/JavaBasics.java
@@ -0,0 +1,10 @@
+
+public class JavaBasics {
+
+    public static void main(String args[]) {
+        System.out.print("Hello Kanan!");
+        System.out.println("Hello Kanan!"); //println func gives one line space after printing
+        System.out.print("Hello Kanan!");
+    }
+}
+//boilerplate code
