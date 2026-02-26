e5c53da9a243b24e8e960b84cdd4701a80bc59fa
Kanan Preet Kaur <kananpreetkaur01@gmail.com>
2026-02-26 19:34:24 +0530

    Update sum.java to take user input using Scanner

--- Java Programming Language/sum.java
+++ Java Programming Language/sum.java
@@ -1,10 +1,15 @@
 
+import java.util.*;
+
 public class sum {
 
     public static void main(String args[]) {
         //code to calculate sum
-        int a = 5;
-        int b = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
     }
