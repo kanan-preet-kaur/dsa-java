50d7a8180191af4c8d9ccf4ae6e00906e1bff332
Kanan Preet Kaur <kananpreetkaur01@gmail.com>
2026-03-04 23:55:16 +0530

    refactor: optimize prime number check using sqrt(n) to reduce time complexity

--- Java Programming Language/prime.java
+++ Java Programming Language/prime.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your number : ");
         int n = sc.nextInt();
-        boolean isPrime = true;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) { //n is a multiple of i, where i is neither 1 nor n
-                System.out.println("Not a prime number");
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
+
+        if (n == 2) {
             System.out.println("It is a prime number");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) { // n is a multiple of i, where i is neither 1 nor n
+                    System.out.println("Not a prime number");
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println("It is a prime number");
+            }
         }
     }
 }
