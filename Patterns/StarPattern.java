3fe481974332683fa49ded73283c715f03aa26a8
Kanan Preet Kaur <kananpreetkaur01@gmail.com>
2026-03-08 19:30:10 +0530

    Add Patterns folder with StarPattern.java - right angled triangle star pattern

---
+++ Patterns/StarPattern.java
@@ -0,0 +1,13 @@
+// Star Pattern
+public class StarPattern {
+    public static void main(String args[]) {
+        //no. of lines
+        for(int i=1;i<=4;i++){
+            //one line
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
