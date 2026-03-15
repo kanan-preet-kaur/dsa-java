8a8cea095ba354646a9a923ba126ea7821d4533d
Kanan Preet Kaur <kananpreetkaur01@gmail.com>
2026-03-15 15:25:40 +0530

    refactor: use method overloading for int and String linear search

--- arrays/LinearSearch.java
+++ arrays/LinearSearch.java
@@ -1,10 +1,22 @@
 public class LinearSearch {
     public static void main(String[] args) {
+
+        //FOR INTEGERS 
         int[] numbers = { 2, 4, 6, 8, 10, 12, 14 };
-        int key = 10;
-        int index = linearSearch(numbers, key);
-        if (index != -1) {
-            System.out.println("The element is present at index : " + index);
+        int intKey = 10;
+        int intIndex = linearSearch(numbers, intKey);
+        if (intIndex != -1) {
+            System.out.println("The element is present at index : " + intIndex);
+        } else {
+            System.out.println("Element not Found");
+        }
+
+        // FOR STRINGS
+        String[] fruits = { "Apple", "Mango", "Guava", "Grapes", "Banana", "Watermelon", "Jackfruit", "Berries" };
+        String stringKey = "Berries";
+        int stringIndex = linearSearch(fruits, stringKey);
+        if (stringIndex != -1) {
+            System.out.println("The element is present at index : " + stringIndex);
         } else {
             System.out.println("Element not Found");
         }
@@ -13,6 +25,15 @@ public class LinearSearch {
     public static int linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int linearSearch(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(key)) {
                 return i;
             }
         }
